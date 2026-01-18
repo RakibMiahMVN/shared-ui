b76d3c8adc9b1ce3314434539cae2142b5d14e35
RakibMiahMVN <rakibmiah@moveon.global>
2026-01-18 12:17:11 +0600

    Remove TimelineComments export from index and update build artifacts

--- src/components/tracking-timeline/index.ts
+++ src/components/tracking-timeline/index.ts
@@ -3,7 +3,6 @@ export { AIPromptInput } from "./AIPromptInput";
 export { default as TrackingTimeline } from "./TrackingTimeline";
 export { default as UserEventCard } from "./UserEventCard";
 export { default as SystemEventCard } from "./SystemEventCard";
-export { default as TimelineComments } from "./TimelineComments";
 export {
   TrackingTimelineProvider,
   useTrackingTimeline,
